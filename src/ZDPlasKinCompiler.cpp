d1495f9f26c6ab0d7287c74b1bc85b6ddb7c15fb
Freek De Sagher <freek.desagher@student.uantwerpen.be>
2021-07-24 16:05:21 +0200

    Implement compile and preprocessor methods
    (currently for Windows only)

--- src/ZDPlasKinCompiler.cpp
+++ src/ZDPlasKinCompiler.cpp
@@ -1,9 +1,10 @@
-#include <sstream>
 #include <iostream>
+#include <filesystem>
 #include "ZDPlasKinCompiler.h"
 #include "ZDPlasKinExceptions.h"
 
 void ZDPlasKinCompiler::preprocess(const std::string &input_file) {
+	// TODO: allow for communication with subprocess if required (pressing enter etc.)
 	std::string command = std::string(PREP_PATH) + " " + input_file + " zdplaskin_m.F90";
 	std::cout << "preprocess command: " << command << std::endl;
 	int retVal = system(command.c_str());
@@ -14,12 +15,21 @@ void ZDPlasKinCompiler::preprocess(const std::string &input_file) {
 }
 
 void ZDPlasKinCompiler::compile(const std::string &opts) {
+	// TODO: extra work for cross-platform support is required here
+	// TODO: create package with ar command (ar cr testlib.a *.o)
 	std::string dvode = std::string(LIB_PATH) + "dvode_f90_m.F90";
-	std::string ZDPlasKin = "./zdplaskin_m.F90";
+	std::string ZDPlasKin = "zdplaskin_m.F90";
 	std::string bolsig = std::string(LIB_PATH) + "bolsig" + std::string(BOLSIG_EXT);
+	std::string defaultOpts = "-O3 -march=native -std=legacy -ffree-line-length-0 ";
+
+	// Copy dll to executable (required in windows)
+	if (!std::filesystem::exists("bolsig" + std::string(BOLSIG_EXT))) {
+		std::filesystem::copy(bolsig, "./bolsig" + std::string(BOLSIG_EXT));
+	}
 
 	std::stringstream ss;
-	ss << "gfortran " << opts << " " << dvode << " " << bolsig << " " << ZDPlasKin << " " << " -o zdplaskin";
+	ss << "gfortran -shared " << defaultOpts << opts << " " << dvode << " " << bolsig << " " << ZDPlasKin << " "
+	   << " -o zdplaskin.dll";
 	std::string command = ss.str();
 	std::cout << "compile command: " << command << std::endl;
 	int retVal = system(command.c_str());
