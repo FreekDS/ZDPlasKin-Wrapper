d15934bde231183afa7b02c93d0b3b343af78ecf
Freek <freek.de.sagher21@gmail.com>
2022-08-21 11:26:40 +0200

    Prepare the preprocess function binding

--- python/bindings.cpp
+++ python/bindings.cpp
@@ -1,10 +1,54 @@
 #include <pybind11/pybind11.h>
+#include <ZDPlasKinWrapper.h>
+#include <ZDPlasKinCompiler.h>
+#include <platform.h>
+#include <iostream>
+#include <filesystem>
+#include "helpers/string_helpers.h"
 
-int add(int i, int j) {
-    return i + j;
+#ifdef WINDOWS
+
+#include <windows/WindowsLoader.h>
+
+using PlatformLoader = WindowsLoader;
+
+#endif
+
+#ifdef UNIX
+
+#include "unix/UnixLoader.h"
+using PlatformLoader = UnixLoader;
+
+#endif
+
+static PlatformLoader libraryLoader{};
+static ZDPlasKinParams params{};
+static ZDPlasKinWrapper zdp{};
+
+namespace py = pybind11;
+
+
+void preprocess(const std::string &kinetFile) {
+    ZDPlasKinCompiler::preprocess(kinetFile);
+    ZDPlasKinCompiler::compile();
+
+    std::string srcPath = std::filesystem::canonical(kinetFile).string();
+    utils::removeSubstr(srcPath, kinetFile);
+    std::string path = srcPath + "zdplaskin.dll";
+
+    libraryLoader = PlatformLoader{path};
+    params = ZDPlasKinParams{srcPath + "zdplaskin_m.F90"};
+
+    zdp.setLibrary(&libraryLoader);
+    zdp.setParams(&params);
+
+    loader.init();
+    parameters.readParams()
 }
 
+
 PYBIND11_MODULE(ZDPlaskin, m) {
+
     m.doc() = R"pbdoc(
         Pybind11 example plugin
         -----------------------
@@ -15,14 +59,14 @@ PYBIND11_MODULE(ZDPlaskin, m) {
            subtract
     )pbdoc";
 
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
-        Some other explanation about the add function.
+    m.def("test", []() { std::cout << "Test from CPP" << std::endl; }, R"pbdoc(
+        Test the CPP bindings
     )pbdoc");
 
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-        Subtract two numbers
-        Some other explanation about the subtract function.
+    m.def("preprocess", &preprocess, R"pbdoc(
+        Preprocess the kinet input file in order to generate the Fortran Library
+        -- kinetFile: absolute path to the input file
     )pbdoc");
+
 }
 
